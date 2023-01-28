a6cfdf47ae53d96025b2ed1c774c3f9e3c0d8190
Priyank Kapadia <priyankkapadia89@gmail.com>
2023-01-28 10:05:33 -0500

    Add Model Validations to Shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -19,14 +19,54 @@
 #  user_id  (user_id => users.id)
 #
 class Shift < ApplicationRecord
+  attribute :active, default: true
+  attribute :start_time, default: Time.now.utc
+
   has_many :breaks
   belongs_to :user
 
-  # validates: :one_active_per_user
-  validates_presence_of: :start_time
+  validate :one_active_per_user
+  validates_presence_of :start_time
+  validate :end_time_gt_start_time
+  validates :active, inclusion: [true, false]
+  validate :end_time_presence
+
+  def clock_start_time!
+    return false if self.end_time.present?
+
+    self.start_time = Time.now.utc
+    self.active = true
+    save!
+  end
+
+  def clock_end_time
+    return false if self.end_time.present?
+
+    self.end_time = Time.now.utc
+    self.active = false
+    save!
+  end
+
+  private
+
+  def end_time_gt_start_time
+    return if self.end_time.blank? && self.start_time.blank?
+    return if self.start_time <= self.end_time
+    errors.add(:base, :end_time, message: "End time cannot be in the past in relation to start time")
+  end
 
   def one_active_per_user
-    errors.add(:shift_already_active, :acitve, "Shift is already active") if active?
+    return if self.end_time.present?
+    errors.add(:active, :more_than_one_active_shift, message: "Shift is already active") if more_than_zero_active
+  end
+
+  def end_time_presence
+    return if active
+    errors.add(:base, :end_time_not_present, message: 'End time should be present') if end_time.blank?
+  end
+
+  def more_than_zero_active
+    Shift.where(user_id: user_id, active: true).count > 0
   end
 
 end
