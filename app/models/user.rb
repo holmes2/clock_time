ef83164d82b476793250b80745ff3f1db57d9daa
Priyank Kapadia <priyankkapadia89@gmail.com>
2023-01-28 15:02:06 -0500

    Add User methods for adding/ending break/shift

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,52 @@ class User < ApplicationRecord
   has_many :shifts
   has_many :breaks, through: :shifts
 
+    def start_shift
+    return if active_shifts?
+
+    shift = Shift.new
+    self.shifts << shift
+    shift.clock_start_time!
+    save!
+  end
+
+  def end_shift
+    active_shift.clock_end_time
+  end
+
+  
+
   def active_shifts?
     shifts.where(active: TRUE).length > 0
+  end
+
+  def start_lunch_break
+    lunch_break = LunchBreak.new
+    active_shift.lunch_break = lunch_break
+    lunch_break.clock_start_time!
+    save!
+  end
+
+  def end_lunch_break
+    active_shift.lunch_break.clock_end_time
+  end
+
+  def start_break
+    normal_break = NormalBreak.new
+    active_shift.breaks << normal_break
+    normal_break.clock_start_time!
+    save!
+  end
+
+  def end_break
+    active_break.clock_end_time
+  end
+
+  def active_break
+    active_shift.breaks.where(active: true).first
+  end
+
+  def active_shift
+    shifts.where(active: true).first
   end
 end
