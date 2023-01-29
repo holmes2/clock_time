5faa09cf4a0b2589278221cd46933d551702e2e8
Priyank Kapadia <priyankkapadia89@gmail.com>
2023-01-29 10:40:31 -0500

    Configure RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :breaks
-  resources :shifts
-  resources :users
+  resources :users do
+    resources :shifts do
+      resources :breaks
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
