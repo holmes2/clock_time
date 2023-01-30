9d15276c9ffc48351fb02b6b6e62803c52345b96
Priyank Kapadia <priyankkapadia89@gmail.com>
2023-01-29 21:35:13 -0500

    Create Well Behaving Views

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,5 +1,5 @@
 class ShiftsController < ApplicationController
-  before_action :set_user, only: %i[ show end_shift start_shift index]
+  before_action :set_user
 
   # GET /shifts or /shifts.json
   def index
@@ -7,17 +7,85 @@ class ShiftsController < ApplicationController
   end
 
   # GET /shifts/1 or /shifts/1.json
-  #def show
-  #end
+  def show
+  end
 
   # GET /shifts/start
   def start_shift
-    @user.start_shift
+    respond_to do |format|
+      if @user.start_shift
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /shifts/end
   def end_shift
-    @user.end_shift
+    respond_to do |format|
+      if @user.end_shift
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /shifts/start_lunch_break
+  def start_lunch_break
+    respond_to do |format|
+      if @user.start_lunch_break
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /shifts/end_lunch_break
+  def end_lunch_break
+    respond_to do |format|
+      if @user.end_lunch_break
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /shifts/start_break
+  def start_break
+    respond_to do |format|
+      if @user.start_break
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /shifts/end_break
+  def end_break
+    respond_to do |format|
+      if @user.end_break
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :users do
     get 'shifts/end', to: 'shifts#end_shift'
     get 'shifts/start', to: 'shifts#start_shift'
+    get 'shifts/start_lunch_break', to: 'shifts#start_lunch_break'
+    get 'shifts/end_lunch_break', to: 'shifts#end_lunch_break'
+    get 'shifts/end_break', to: 'shifts#end_break'
+    get 'shifts/start_break', to: 'shifts#start_break'
+
     resources :shifts, only: [:show, :index] do
       resources :breaks, only: [:create, :index, :update, :show, :new]
     end
